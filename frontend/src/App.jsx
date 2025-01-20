0f1971329e8a91cb20e62eadb817740f26125559
maridi manoj kumar <21a91a6152@aec.edu.in>
2025-01-20 14:13:34 +0530

    commit8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Home from './components/home';
@@ -8,9 +8,14 @@ import Contact from "./components/contact";
 import MyTeam from "./components/myteam";
 import ClassDetails from "./components/classes";
 import SignIn from './components/login';
-import { useState ,useEffect} from 'react';
+import { useState, useEffect } from 'react';
+
 function App() {
   const [user, setUser] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    Boolean(localStorage.getItem("userData"))
+  );
+
   useEffect(() => {
     const userDataString = localStorage.getItem('userData');
     if (userDataString) {
@@ -18,9 +23,6 @@ function App() {
       setUser(userData);
     }
   }, []);
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    Boolean(localStorage.getItem("userData"))
-  );
 
   // Function to handle logout
   const handleLogout = () => {
@@ -32,38 +34,31 @@ function App() {
   const handleLogin = () => {
     setIsAuthenticated(true);
   };
- 
+
   return (
-    <>
-      <Router>
+    <Router>
       {isAuthenticated ? (
-        <> 
-            <Navbar onLogout={handleLogout} />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about-us" element={<About />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/team" element={<MyTeam />} />
-              <Route path="/classes" element={<ClassDetails />} />
-              <Route path='/login' element={<SignIn/>}/>
-            </Routes>
-            <Footer />
-            </>
-          ) : (
-            <Routes>
-              <Route path="/login" element={<SignIn onLogin={handleLogin} />} />
-              <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect to login */}
-            </Routes>
-          )}
-          
-      </Router>
-    </>
-  )
+        <>
+          <Navbar onLogout={handleLogout} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/team" element={<MyTeam />} />
+            <Route path="/classes" element={<ClassDetails />} />
+            <Route path="/login" element={<SignIn />} />
+          </Routes>
+          <Footer />
+        </>
+      ) : (
+        <Routes>
+          <Route path="/login" element={<SignIn onLogin={handleLogin} />} />
+          <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect to login */}
+        </Routes>
+      )}
+    </Router>
+  );
 }
 
-
 export default App;
-
-
-  
